bfe436ab4418d309ace23dd76d2892ea8095edf4
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2024-07-27 15:55:00 +0530

    Update settings.py

--- aone/settings.py
+++ aone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%qc$v4m1ep^)q4tys9ja=&qipjx)25et5+$!+qhg+50do@wsl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
