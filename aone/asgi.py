bfa19da56bc3b1581c68ceeb882d7b0348cfb9f2
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2024-07-29 08:54:03 +0530

    Update asgi.py

--- aone/asgi.py
+++ aone/asgi.py
@@ -4,8 +4,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aone.settings")
+django.setup()
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
